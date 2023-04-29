52d2eda20c99808ccbd04246c99acb1960a4fd85
daanmgneni <94113582+daanmgneni@users.noreply.github.com>
2023-04-29 08:52:41 +0530

    Update EmployeePayroll.sql

--- EmployeePayroll.sql
+++ EmployeePayroll.sql
@@ -10,9 +10,14 @@ create table employee_payroll  /* create table */
 select * from employee_payroll
 
 /* insert values */
-insert into employee_payroll values('Ram', 50000, '2023-04-21'),
-                                   ('Shyam', 20000, '2023-04-21'),
-								   ('Bhole', 70000, '2023-04-21'),
-								   ('Durga', 50000, '2023-04-21')
+insert into employee_payroll values('Ram', 50000, '2003-04-2'),
+                                   ('Shyam', 20000, '2013-03-1'),
+								   ('Bhole', 70000, '2020-09-21'),
+								   ('Durga', 50000, '2023-01-11')
 
 select * from employee_payroll  /* retrive data */
+
+select Salary from employee_payroll where id = 3   /* salary of particular employee */
+
+select * from employee_payroll where StartDate BETWEEN CAST('2013-03-01' AS DATE) AND GETDATE() /* employee deatil b/w date range */
+
