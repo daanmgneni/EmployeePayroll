e40d3664fa801b23043b1e910d967057fe71c0d0
daanmgneni <94113582+daanmgneni@users.noreply.github.com>
2023-04-29 08:51:32 +0530

    Update EmployeePayroll.sql

--- EmployeePayroll.sql
+++ EmployeePayroll.sql
@@ -1,7 +1,5 @@
-
-11 lines (9 loc) · 225 Bytes
              /*!!!!! Welcome to EmployeePayrollProblem !!!!*/
-create table employee_payroll
+create table employee_payroll  /* create table */
 (
  Id int primary key identity,
  Name varchar(40) NOT NULL,
@@ -11,3 +9,10 @@ create table employee_payroll
 
 select * from employee_payroll
 
+/* insert values */
+insert into employee_payroll values('Ram', 50000, '2023-04-21'),
+                                   ('Shyam', 20000, '2023-04-21'),
+								   ('Bhole', 70000, '2023-04-21'),
+								   ('Durga', 50000, '2023-04-21')
+
+select * from employee_payroll  /* retrive data */
